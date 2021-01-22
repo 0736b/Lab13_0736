26c7fafa8fc73f5ee933ae8ca06d2adf229c9f05
0736b <thanawat_bumpengpun@elearning.cmu.ac.th>
2021-01-22 17:15:00 +0700

    lab13_2.cpp completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -19,3 +19,47 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void showMatrix(const bool matrix[][N]){
+for(int i = 0; i< N; i++){
+	for(int j = 0 ; j<N; j++){
+		cout << matrix[i][j] << " ";
+	}
+	cout << endl;
+}
+}
+
+void inputMatrix(double input_matrix[][N]){
+	for(int i = 0; i < N; i++){
+			cout << "Row " << i+1 << ": ";
+			for(int j = 0; j<N; j++){
+				cin >> input_matrix[i][j];
+			}
+			}
+	}
+
+void findLocalMax(const double M_Input[][N], bool M_Output[][N]){
+	
+	// สมาชิกขอบบน ล่าง ซ้าย ขวา ของ B จะมีค่าเท่ากับ 0
+	for(int i = 0; i < N; i++){
+		M_Output[0][i] = 0;
+		M_Output[N-1][i] = 0;
+		
+	}
+	for(int j = 1 ; j < N; j++){
+		M_Output[j][0] = 0;
+		M_Output[j][N-1] = 0;
+	}
+    // เช็ค Matrix A รอบข้าง 4 ทิศ
+
+    	for(int i = 1 ; i < N-1; i++){
+		for(int j = 1 ; j< N-1; j++){
+			if((M_Input[i][j] >= M_Input[i-1][j]) && (M_Input[i][j] >= M_Input[i][j+1]) && (M_Input[i][j] >= M_Input[i+1][j]) && (M_Input[i][j] >= M_Input[i][j-1])){
+				M_Output[i][j] = 1;
+			}
+			else if((M_Input[i][j] < M_Input[i-1][j]) || (M_Input[i][j] < M_Input[i][j+1]) || (M_Input[i][j] < M_Input[i+1][j]) || (M_Input[i][j] < M_Input[i][j-1])){
+				M_Output[i][j] = 0;
+			}
+		}
+	}
+}
